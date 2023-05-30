7a1083f192735e0c281ebbc81f19055dd5af9700
Mahesh-Anand <pvmaheshanand3030@gmail.com>
2023-05-30 11:42:18 +0530

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,17 +8,26 @@ import Search from "./components/Search"
 //   console.log(e.target[0].value)
   
 // } //move this to  App as this is working inside it or after it
- 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
 function App() {
   const [word, setWord] = useState('');
 
   const handleSearchSubmit = (e) =>{
-    e.preventDefault()
+    e.preventDefault() // To remove refreshing the content so that we can take the values
    
     console.log(word)
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    .then((res)=>res.json())
+    .then((data)=>{
+      console.log(data);
+        })
+    .catch((err)=>{
+      console.log(err);
+    })
     
   }
-  console.log(word) 
+  // console.log(word) 
+  console.log(process.env.REACT_APP_UNSPLASH_KEY) 
     
   return (
     
