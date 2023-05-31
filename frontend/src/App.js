c960f91c3d36a3326baa0e4a2b8ef44d1a111d6c
Mahesh-Anand <pvmaheshanand3030@gmail.com>
2023-05-31 11:40:57 +0530

    save images in the state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
 
-// const handleSearchSubmit = (e) =>{
-//   e.preventDefault()
-//   console.log(e.target[0].value)
 
-// } //move this to  App as this is working inside it or after it
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 function App() {
   const [word, setWord] = useState("");
+  const [images, setImages] = useState([]);
 
+  // console.log(images);
+  
   const handleSearchSubmit = (e) => {
     e.preventDefault(); // To remove refreshing the content so that we can take the values
 
@@ -21,13 +20,14 @@ function App() {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([data,...images])
+        
       })
       .catch((err) => {
         console.log(err);
       });
 
-      setWord("")
+    setWord("");
   };
   // console.log(word)
   // console.log(process.env.REACT_APP_UNSPLASH_KEY);
@@ -38,6 +38,7 @@ function App() {
     <div>
       <Header title="Image Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      {/* <MyComponent /> */}
     </div>
   );
 }
