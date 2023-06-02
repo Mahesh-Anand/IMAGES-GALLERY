05d386b4034f658b865a3855d2472a35a9ffd4f1
Mahesh-Anand <pvmaheshanand3030@gmail.com>
2023-06-02 16:23:13 +0530

    sample flask app

--- api/main.py
+++ api/main.py
@@ -1,1 +1,26 @@
-print("Hello from python")
+# save this as app.py
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/hi")
+def hello():
+    return "Hello, World!"
+
+# or
+# app.route("/")(hello)# telling app to get hello funtion in this file to be executed
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello():
+#     return "Hello, World!"
+
+# @app.route("/about")
+# def about():
+#     return "This is the About page."
+
+# if __name__ == "__main__":
+#     app.run()
